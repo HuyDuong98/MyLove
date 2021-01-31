1dc123d007211ed123b39e441b9adbb004505bef
HuyDuong98 <54093915+HuyDuong98@users.noreply.github.com>
2021-01-31 08:53:24 +0700

    MUSIC

--- src/js/musicplayer.js
+++ src/js/musicplayer.js
@@ -1,11 +1,13 @@
 $(function()
 {
-    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false, albums = ['Dawn','Me & You','Electro Boy','Home','Proxy (Original Mix)'], trackNames = ['Skylike - Dawn','Alex Skrindo - Me & You','Kaaze - Electro Boy','Jordan Schor - Home','Martin Garrix - Proxy'], 
+    var playerTrack = $("#player-track"), bgArtwork = $('#bg-artwork'), bgArtworkUrl, albumName = $('#album-name'), trackName = $('#track-name'), albumArt = $('#album-art'), sArea = $('#s-area'), seekBar = $('#seek-bar'), trackTime = $('#track-time'), insTime = $('#ins-time'), sHover = $('#s-hover'), playPauseButton = $("#play-pause-button"),  i = playPauseButton.find('i'), tProgress = $('#current-time'), tTime = $('#track-length'), seekT, seekLoc, seekBarPos, cM, ctMinutes, ctSeconds, curMinutes, curSeconds, durMinutes, durSeconds, playProgress, bTime, nTime = 0, buffInterval = null, tFlag = false,
+     albums = ['3107','Nàng Thơ','Electro Boy','Home','Proxy (Original Mix)'], 
+     trackNames = ['W/n, Duongg, Nâu','Hoàng Dũng','Phố Cũ Còn Anh','Jordan Schor - Home','Martin Garrix - Proxy'], 
     albumArtworks = ['_1','_2','_3','_4','_5'], 
     trackUrl = [
-        'https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/2.mp3',
-    'https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/1.mp3',
-    'https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/3.mp3',
+    'mp3/3-1-0-7-Duongg-Nau.mp3',
+    'mp3/NangTho-HoangDung-6413381.mp3',
+    'mp3/PhoCuConAnh-QuinnChilly-6791854.mp3',
     'https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3',
     'https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/5.mp3'], 
     playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
